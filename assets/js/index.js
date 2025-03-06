272ad3cb54cb8f8864e309cc4863fae0c7d2d24f
nemoto <haikonggenben@gmail.com>
2025-03-06 19:58:12 +0800

    yes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,36 @@
-//topページ
-$(".to-top").on("click", function () {
-  $("html, body").animate({ scrollTop: 0 }, 100); // 500はスクロールの速さ（ミリ秒）
+$(window).on("load", function () {
+  // ページ読み込み完了後にローディング画面をフェードアウト
+  setTimeout(function () {
+      $("#loading-screen").addClass("fade-out");
+  }, 1500); // 1.5秒後にフェードアウト開始
+
+  setTimeout(function () {
+      $("#loading-screen").remove(); // フェードアウト後に削除
+  }, 2000); // 2秒後に完全に削除
 });
+//topページ
 $(window).on("scroll", function () {
   let imageContainer = $(".img-farst");
-  let windowHeight = window.innerHeight; // ウィンドウの高さを取得
+  let windowHeight = window.innerHeight; 
   let scrollTop = $(window).scrollTop();
   let imageTop = imageContainer.offset().top;
   let imageHeight = imageContainer.outerHeight();
 
-  // スクロール位置の判定
+
   if (scrollTop + windowHeight > imageTop + imageHeight / 2) {
-    $(".text-farst").fadeIn(300); // スマホでも早く表示されるように
+    $(".text-farst").fadeIn(300); 
   } else {
     $(".text-farst").fadeOut(300);
   }
 });
 
 $(document).ready(function () {
-  let scrolled = false; // スクロール開始を1回だけ検知するためのフラグ
+  let scrolled = false; 
 
   $(window).on("scroll", function () {
     if (!scrolled) {
-      $(".img-farst img").css("border-radius", "5%"); // アニメーション実行
-      scrolled = true; // 2回目以降は動作しないようにする
+      $(".img-farst img").css("border-radius", "5%"); 
+      scrolled = true; 
     }
   });
   $(document).ready(function () {
@@ -60,14 +67,7 @@ $(document).ready(function () {
     );
   }, 1500);
 });
-$(window).on("load", function () {
-  // ページ読み込み完了後にローディング画面をフェードアウト
-  setTimeout(function () {
-      $("#loading-screen").addClass("fade-out");
-  }, 1500); // 1.5秒後にフェードアウト開始
-
-  setTimeout(function () {
-      $("#loading-screen").remove(); // フェードアウト後に削除
-  }, 2000); // 2秒後に完全に削除
+$(".to-top").on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, 100); 
 });
 
